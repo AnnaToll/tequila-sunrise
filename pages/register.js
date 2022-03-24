fd6639fc677e98c385e87daa0fe91aa66d6cbb0b
JesperHagman <jesper.hagman98@gmail.com>
2022-03-24 13:46:35 +0100

    added backend

--- pages/register.js
+++ pages/register.js
@@ -1,28 +1,52 @@
 const Register = () => {
+
+  const [details, setDetails] = useState({name: "", email: "", password: "", phone: ""})
+
+  const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      const user = {
+        name: details.name,
+        phone: details.phone,
+        email: details.email,
+        password: details.password
+      }
+    
+      fetch('/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+      })
+      .then(res => res.json())
+      .then(data => console.log(data));
+    }
+
     return ( 
         <form>
-        <div className="form-inner">
+        <div className="form-inner" onClick={handleSubmit}>
 
             <h2>Register</h2> 
 
             <div className="form-group">
                 <label htmlFor='name'>Name:</label>
-                <input type="text" name="name" id="name" required/>
+                <input type="" name="name" id="name" required onChange={e =>setDetails({...details, name: e.target.value})} value={details.name}/>
             </div>
 
             <div className="form-group">
-                <label htmlFor='lastname'>Lastname:</label>
-                <input type="text" lastname="lastname" id="lastname" required/>
+                <label htmlFor='phone'>Phone number:</label>
+                <input type="number" phone="phone" id="phone" required onChange={e =>setDetails({...details, phone: e.target.value})} value={details.phone}/>
             </div>
 
             <div className="form-group">
                 <label htmlFor='email'>Email:</label>
-                <input type="text" email="email" id="email" required/>
+                <input type="text" email="email" id="email" required onChange={e =>setDetails({...details, email: e.target.value})} value={details.email}/>
             </div>
 
             <div className="form-group">
                 <label htmlFor='password'>Password:</label>
-                <input type="password" password="password" id="password" required/>
+                <input type="password" password="password" id="password" required onChange={e =>setDetails({...details, password: e.target.value})} value={details.password}/>
             </div>
 
             <input type="submit" value="REGISTER" />
