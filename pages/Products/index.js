086588633e2c331b8726d428134927f0aaeb679c
Johan Blomberg <Johanblomberg89@gmail.com>
2022-03-25 19:31:43 +0100

    created loop for products

--- pages/Products/index.js
+++ pages/Products/index.js
@@ -1,19 +1,24 @@
+import React, { useState, useEffect } from 'react'
+
 const ProductPage = () => {
 
-    const getAllProducts = async => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
         fetch("/api/products")
-            .then((response) => {
-              return response.json()
-          })
-          .then((data) => {
-              console.log(data)
-              })
-      }
-    
+        .then((response) => {
+         return response.json();
+      })
+      .then((data) => {
+        setProducts(data)
+        console.log(data)
+      } )
+
+    }, []) 
     return ( 
         <main>
         <div className="filterProducts">
-            <button onClick={getAllProducts}>Tryck</button>
+          
             <h1>Tech-ila</h1>
             <ul>
                 <li>Filter 1</li>
@@ -22,13 +27,17 @@ const ProductPage = () => {
                 <li>Filter 4</li>
             </ul>
         </div>
-        <div className="productPage">
-            <h3>Product:</h3>
-            <h3>Pris:</h3>
-            <h3>Land:</h3>
-            <h3>Procent:</h3>
-            <h3>IMAGE</h3>
-            
+        <div>
+        <h1>Alla grejer</h1>
+        {products.map((product) => (
+            <div key={product.id}>
+                <img src={`IMG/Products/${product.image}`}></img>
+                <p>{product.name}</p>
+                <p>{product.description}</p>
+                <p>{product.country}</p>
+                <i>{product.price}</i>
+            </div>
+        ))}
         </div>
         </main>
      );
