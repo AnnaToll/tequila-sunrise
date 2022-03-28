318a1bc6442790590f0230d107703a12a57e7c79
Johan Blomberg <Johanblomberg89@gmail.com>
2022-03-28 17:58:23 +0200

    Created header v1

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,28 @@
+import Link from "next/link";
+import logo from "../public/IMG/Logo/Tech-ila.PNG"
+
 const Header = () => {
     return ( 
-        <div></div>
+        <nav>
+            <img src={logo}></img>
+            <ul>
+            <li>
+           <Link href="/">
+               <a>Home</a>
+           </Link>
+           </li>
+           <li>
+           <Link href="/Products">
+               <a>Products</a>
+           </Link>
+           </li>
+           <li>
+           <Link href="/Products">
+               <a><button>Logga in</button></a>
+           </Link>
+           </li>
+           </ul>
+        </nav>
      );
 }
  
