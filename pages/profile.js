e3b311212533d1bb55b527ad63d2cdce9c7990a3
Jesper Hagman <Jesper.hagman98@gmail.com>
2022-03-29 14:00:09 +0200

    cleaning up the code a bit

--- pages/profile.js
+++ pages/profile.js
@@ -3,41 +3,36 @@ import { useRouter } from 'next/router'
 
 
 export default function Register() {
-  const router = useRouter()
   const [loggedIn, setLoggedIn] = useState(false);
+  const router = useRouter()
+  
+  useEffect(() => {
+   const coockie = localStorage.getItem('isLoggedIn');
+   console.log(coockie)
+   if (coockie)
+     setLoggedIn(true);
+ }, [])
 
-    const logout = () => {
-       localStorage.removeItem('isLoggedIn')
-       setLoggedIn(false);
-       router.push('/')
-    }
+  const logout = () => {
+    localStorage.removeItem('isLoggedIn')
+    setLoggedIn(false);
+    router.push('/')
+  }
 
-     useEffect(() => {
-      const test = localStorage.getItem('isLoggedIn');
-      if (test)
-        setLoggedIn(true);
-    }, [])
-
-      
-        if(loggedIn) {
-          return ( //If user is signed in this shows
-          <div className="welcome">
-            <h1>Welcome User</h1>
-            <button onClick={logout}> Logout </button>
-          </div>
-          )
-      } else {
-          return ( // If user isn´t singed in this shows
+  if(loggedIn) {
+    return ( //If user is signed in this shows
+      <div className="welcome">
+        <h1>Welcome User</h1>
+        <button onClick={logout}> Logout </button>
+      </div>
+    )
+    } else {
+        return ( // If user isn´t singed in this shows
           <div className='welcome'>
             <h2> Please Sign in or register before visiting this page </h2>
             <button></button> 
-            </div>
+          </div>
           )
-      
-
-      }
-
-
-   
-}
+        } 
+     }
   
