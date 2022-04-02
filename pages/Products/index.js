d0c41ac4d148ad4e4e5af5a915299059ef0b2ccd
Johan Blomberg <JohanBlomberg89@gmail.com>
2022-04-02 21:44:30 +0200

    Added "addtocart" module to product Index

--- pages/Products/index.js
+++ pages/Products/index.js
@@ -1,21 +1,49 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import Link from 'next/link'
 import styles from '../../styles/Products.module.css'
+import PutInCart from '../../components/PutInCart';
+import { useRouter } from 'next/router';
 
 const ProductPage = () => {
 
-    const addToCart = (e) => {
-        e.preventDefault();
-        console.log("Hej!")
-    }
-
-    const changeFilter = (event) => {
-        if(event.target.value === "Odefinerad"){
-            console.log("Tjena!")
-        }
-    }
-
     const [products, setProducts] = useState([]);
+    const [productData, setProductData] = useState();
+    const [quantity, setQuantity] = useState(1);
+    const router = useRouter();
+
+    const getDataFromDB = useCallback(async () => {
+        const { id } = router.query;
+        if (id) {
+            try {
+                const response = await fetch("/api/product/" + id);
+                const data = await response.json();
+                return setProductData(data);
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }, [router]);
+
+    useEffect(() => {
+        getDataFromDB();
+    }, [getDataFromDB, router]);
+
+    const changeFilter = (filters) => {
+        console.log(filters)
+        
+        const differentFilters  = {
+            price: 'price'
+        };
+
+        const sortFilter = differentFilters[filters];
+        const sorted = [...products].sort((a, b) => b[sortFilter] - a[sortFilter])
+            setProducts(sorted)
+
+        
+          console.log(sorted)
+    }
+
 
     useEffect(() => {
         fetch("/api/products")
@@ -36,11 +64,11 @@ const ProductPage = () => {
             <p>Vår tequila skall helst avnjutas rumstempererad och utan tillbehör, njut t.ex. av Röda hatten en varm sommardag med några vänner och en skön minneslucka. </p>
 
             <div className={styles.filterProducts}>          
-          <label htmlFor="productFilter">
-              <select name="productFilter" onChange={changeFilter} className={styles.productFilter}>
+          <label htmlFor="productFilter" className={styles.labelFilter}>
+              <select name="productFilter" onChange={(e) => changeFilter(e.target.value)} className={styles.productFilter}>
                   <option value="Odefinerad">Välj ett filter</option>
-                  <option value="Högsta till lägsta">Högsta till lägsta</option>
-                  <option value="Lägsta till högsta">Lägsta till högsta</option>
+                  <option value="price">Högsta till lägsta</option>
+                  <option value="price">Lägsta till högsta</option>
               </select>
           </label>
   </div>
@@ -53,7 +81,11 @@ const ProductPage = () => {
                       <img src={`IMG/Products/${product.image}`} className={styles.productImage}></img>
                      <p>Ursprungsland: {product.country}</p>
                      <p>Pris: {product.price}:-</p>
-                    <button className='addToCart' onClick={addToCart}>Lägg till i varukorg</button>
+                     <PutInCart
+                        quantity={quantity}
+                        productData={productData}
+                        onChange={(e) => setQuantity(+ e.target.value)}
+                        type="number"/>
                 </div>
                         </a></Link>
                 ))}
