506d8c80ab9d136c1effa1b19855b335cffd9d84
Isabelle <isabelle.annie.karlsson@gmail.com>
2022-04-05 13:57:07 +0200

    Minor change

--- components/PutInCart.js
+++ components/PutInCart.js
@@ -1,5 +1,5 @@
 import { ADD_ITEM } from '/redux/actions/actionTypes';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch /*useSelector*/ } from 'react-redux';
 import styles from '../styles/Product.module.css';
 
 function PutInCart(props) {
