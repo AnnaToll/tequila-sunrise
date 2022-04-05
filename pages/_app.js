8554c6c188159fdccfacf9487df0fbe07732fa4d
Johan Blomberg <JohanBlomberg89@gmail.com>
2022-04-05 10:13:18 +0200

    Fixed app

--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,19 @@ import '../styles/globals.css'
 import { wrapper } from '../redux/store';
 import Header from '../components/Header';
 import { useStore } from 'react-redux';
-import CookiesAndPersist from '../components/CookiesAndPersist';
 import Footer from '../components/Footer'
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
 function MyApp({ Component, pageProps }) {
 
   const store = useStore();
 
   return (
-    <CookiesAndPersist loading={null} persistor={store._persistor}>
+    <PersistGate loading={null} persistor={store._persistor}>
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </CookiesAndPersist>
+    </PersistGate>
   );
 }
 
