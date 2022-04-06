9ca7c680fcf798e029ac9404cbeba5d28cec006a
Isabelle <isabelle.annie.karlsson@gmail.com>
2022-04-06 20:58:23 +0200

    Added error handling and removed useCallback

--- pages/api/product/[id].js
+++ pages/api/product/[id].js
@@ -14,6 +14,6 @@ export default async function handler(req, res) {
 
     } catch (error) {
         console.log(error);
-        // res.status(500).json({ message: error});
+        res.status(500).json({ message: error });
     }
 }
